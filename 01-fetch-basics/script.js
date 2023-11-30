8987adcec5d6ec2471bb17ccdb1904c75ebbdecb
Anxious Ape <iamkhidash@gmail.com>
2023-11-30 12:10:21 +0530

    Same in AJAX

--- 01-fetch-basics/script.js
+++ 01-fetch-basics/script.js
@@ -19,8 +19,47 @@ const ul = document.querySelector("#verthe-ul");
 //   .then((data) => console.log(data));
 
 //? fetch from git-API
-fetch("https://api.github.com/users/khidash8/repos")
-  .then((user) => user.json())
+// fetch("https://api.github.com/users/khidash8/repos")
+//   .then((user) => user.json())
+//   .then((data) => {
+//     data.forEach((element) => {
+//       const li = document.createElement("li");
+//       li.innerHTML = `${element.name}`;
+//       li.style.fontSize = "1.3rem";
+//       ul.appendChild(li);
+//     });
+//   });
+
+// fetch("https://api.github.com/users/khidash8")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     document.querySelector("h1").textContent = data.login;
+//   });
+
+//? ajax promis
+const xhr = new XMLHttpRequest();
+
+function getUser(endpoint) {
+  xhr.open("GET", endpoint);
+  return new Promise((resolve, reject) => {
+    xhr.onreadystatechange = function () {
+      if (this.readyState === 4) {
+        if (this.status === 200) {
+          const data = JSON.parse(this.response);
+          resolve(data);
+        } else {
+          reject("something went wrong!!");
+        }
+      }
+    };
+  });
+}
+
+setTimeout(() => {
+  xhr.send();
+}, 1000);
+
+getUser("https://api.github.com/users/khidash8/repos")
   .then((data) => {
     data.forEach((element) => {
       const li = document.createElement("li");
@@ -28,10 +67,5 @@ fetch("https://api.github.com/users/khidash8/repos")
       li.style.fontSize = "1.3rem";
       ul.appendChild(li);
     });
-  });
-
-fetch("https://api.github.com/users/khidash8")
-  .then((res) => res.json())
-  .then((data) => {
-    document.querySelector("h1").textContent = data.login;
-  });
+  })
+  .catch((error) => console.log(error));
